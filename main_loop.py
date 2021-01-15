34b5b5454c1da009089a56147bd88e44d80f5f79
SLotAbr <49324598+SLotAbr@users.noreply.github.com>
2021-01-15 21:14:51 +0300

    Update main_loop.py

--- main_loop.py
+++ main_loop.py
@@ -6,31 +6,34 @@ from models import Decoder_model
 
 save_folder = 'parameters/'
 source = open('input.txt', 'r').read()
+source = source.split()
 alphabet = list(set(source))
 source_lenght, vocabulary_size = len(source), len(alphabet)
 letter_transform = { letter:i for i, letter in enumerate(alphabet) }
 indexes_transform = { i:letter for i, letter in enumerate(alphabet) }
+
+print('vocabulary_size: ', vocabulary_size)
 # if some files will be found in "save_folder" folder,
 # they will be used as the last checkpoint
 if len(os.listdir(path=save_folder))!=0:
 	with open(save_folder+'model_decription.pkl', 'rb') as f:
-		context_size, d_model, H, optim_param = pickle.load(f)
+		context_size, d_model, H, N, optim_param = pickle.load(f)
 	with open(save_folder+'iteration_param.pkl', 'rb') as f:
 		s, step_num, loss, lr = pickle.load(f)
 
-	model = Decoder_model(context_size, vocabulary_size, d_model, H, optim_param)
+	model = Decoder_model(context_size, vocabulary_size, d_model, H, N, optim_param)
 	model.restore_parameters(save_folder)
 else:
 	s, step_num, loss = 0, 0, 0
-	context_size, d_model, H = 128, 128, 2
+	context_size, d_model, H, N = 32, 64, 2, 3
 	lr = 1e-6
 	optim_param = [lr, 0.9, 0.98] # lr, b1, b2
 	with open(save_folder+'model_decription.pkl', 'wb') as f:
-		pickle.dump([context_size, d_model, H, optim_param], f)
-	model = Decoder_model(context_size, vocabulary_size, d_model, H, optim_param)
+		pickle.dump([context_size, d_model, H, N, optim_param], f)
+	model = Decoder_model(context_size, vocabulary_size, d_model, H, N, optim_param)
 
-threshold = 45000
-checkpoint = step_num
+threshold = 30000
+checkpoint = step_num 
 checkpoint_loss = 1e12
 print('preparation\'s complete!')
 
@@ -51,23 +54,23 @@ while True:
 
 		index_list=[np.random.randint(0, vocabulary_size)]
 		target_list=[]
-		for l in range(127):
+		for l in range(31):
 			index_list.append(model.forward(index_list,target_list, phase='eval'))
 		
-		text_example = ''.join(indexes_transform[i] for i in index_list)
+		text_example = ' '.join(indexes_transform[i] for i in index_list)
 		print('--------\n %s \n--------' % (text_example, ))
-		print('iter %d, loss: %f' % (step_num, loss, ))
-		print('lr: ',lr)
+		print('iter %d, loss: %f, lr: %g' % (step_num, loss, lr))
 	
-	if loss <= checkpoint_loss:
-		checkpoint = step_num
-		checkpoint_loss = loss
-	elif (step_num - checkpoint) > threshold:
-		lr/= 10
-		model.change_lr(lr)
-		print('lr has been reduced. New value: ', lr)
-		checkpoint = step_num
-		checkpoint_loss = loss
+	if step_num > 10000: # We'll change loss only after 1e4 iteration
+		if loss <= checkpoint_loss:
+			checkpoint = step_num
+			checkpoint_loss = loss
+		elif (step_num - checkpoint) > threshold:
+			lr/= 10
+			model.change_lr(lr)
+			print('lr has been reduced. New value: ', lr)
+			checkpoint = step_num
+			checkpoint_loss = loss
 
 	step_num += 1
 	s += context_size
