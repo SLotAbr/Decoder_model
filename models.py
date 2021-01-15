ff8bece74b385f42ffaa05b290e5327112112776
SLotAbr <49324598+SLotAbr@users.noreply.github.com>
2021-01-15 21:16:08 +0300

    Multilayer update

--- models.py
+++ models.py
@@ -10,50 +10,69 @@ from tools.optimizers import AdaM
 
 
 class Decoder_model:
-	def __init__(self, context_size, vocabulary_size, d_model, H, optim_param):
+	def __init__(self, context_size, vocabulary_size, d_model, H, N, optim_param):
 		self.TE = nn.token_embedding(vocabulary_size, d_model, context_size, optim_param)
 		self.PE = positional_encoding(context_size, d_model)
 
-		self.W_attention = nn.linear(d_model, d_model*3, optim_param)
-		self.MH_attention = nn.MH_attention_mechanism(context_size, d_model, H)
-		self.W_heads_projection = nn.linear(d_model, d_model, optim_param)
-		self.Attention_LayerNorm = nn.LayerNormalization(context_size)
+		self.W_attention = [0 for n in range(N)]
+		self.MH_attention = [0 for n in range(N)]
+		self.W_heads_projection = [0 for n in range(N)]
+		self.Attention_LayerNorm = [0 for n in range(N)]
 
-		self.W_FC1 = nn.linear(d_model, d_model*4, optim_param)
-		self.activation = nn.ReLU()
-		self.W_FC2 = nn.linear(d_model*4, d_model, optim_param)
-		self.FC_LayerNorm = nn.LayerNormalization(context_size)
+		self.W_FC1 = [0 for n in range(N)]
+		self.activation = [0 for n in range(N)]
+		self.W_FC2 = [0 for n in range(N)]
+		self.FC_LayerNorm = [0 for n in range(N)]
+		for n in range(N):
+			self.W_attention[n] = nn.linear(d_model, d_model*3, optim_param)
+			self.MH_attention[n] = nn.MH_attention_mechanism(context_size, d_model, H)
+			self.W_heads_projection[n] = nn.linear(d_model, d_model, optim_param)
+			self.Attention_LayerNorm[n] = nn.LayerNormalization(context_size)
+
+			self.W_FC1[n] = nn.linear(d_model, d_model*4, optim_param)
+			self.activation[n] = nn.ReLU()
+			self.W_FC2[n] = nn.linear(d_model*4, d_model, optim_param)
+			self.FC_LayerNorm[n] = nn.LayerNormalization(context_size)
 
 		self.Output_token_probabilities = 0
 		self.context_size = context_size
+		self.N = N
 		self.vocabulary_size = vocabulary_size
 		self.residual_backprop = np.ones((context_size, d_model))
 
 	def save_parameters(self, folder):
 		# The folder must ends on '/'!
 		self.TE.save_weights(folder+'TE_param.pkl')
-		self.W_attention.save_weights(folder+'W_attention_param.pkl')
-		self.W_heads_projection.save_weights(folder+'W_heads_projection_param.pkl')
-		self.W_FC1.save_weights(folder+'W_FC1_param.pkl')
-		self.W_FC2.save_weights(folder+'W_FC2_param.pkl')
+
+		for n in range(self.N):
+			self.W_attention[n].save_weights(folder+'W_attention_param{}.pkl'.format(str(n)))
+			self.W_heads_projection[n].save_weights\
+						(folder+'W_heads_projection_param{}.pkl'.format(str(n)))
+			self.W_FC1[n].save_weights(folder+'W_FC1_param{}.pkl'.format(str(n)))
+			self.W_FC2[n].save_weights(folder+'W_FC2_param{}.pkl'.format(str(n)))
 
 	def restore_parameters(self, folder):
 		self.TE.restore_weights(folder+'TE_param.pkl')
-		self.W_attention.restore_weights(folder+'W_attention_param.pkl')
-		self.W_heads_projection.restore_weights(folder+'W_heads_projection_param.pkl')
-		self.W_FC1.restore_weights(folder+'W_FC1_param.pkl')
-		self.W_FC2.restore_weights(folder+'W_FC2_param.pkl')
+
+		for n in range(self.N):
+			self.W_attention[n].restore_weights(folder+'W_attention_param{}.pkl'.format(str(n)))
+			self.W_heads_projection[n].restore_weights\
+						(folder+'W_heads_projection_param{}.pkl'.format(str(n)))
+			self.W_FC1[n].restore_weights(folder+'W_FC1_param{}.pkl'.format(str(n)))
+			self.W_FC2[n].restore_weights(folder+'W_FC2_param{}.pkl'.format(str(n)))
 
 	def change_lr(self, lr):
 		self.TE.optim.lr = lr
-		self.W_attention.w_optim.lr = lr
-		self.W_attention.b_optim.lr = lr
-		self.W_heads_projection.w_optim.lr = lr
-		self.W_heads_projection.b_optim.lr = lr
-		self.W_FC1.w_optim.lr = lr
-		self.W_FC1.b_optim.lr = lr
-		self.W_FC2.w_optim.lr = lr
-		self.W_FC2.b_optim.lr = lr
+
+		for n in range(self.N):
+			self.W_attention[n].w_optim.lr = lr
+			self.W_attention[n].b_optim.lr = lr
+			self.W_heads_projection[n].w_optim.lr = lr
+			self.W_heads_projection[n].b_optim.lr = lr
+			self.W_FC1[n].w_optim.lr = lr
+			self.W_FC1[n].b_optim.lr = lr
+			self.W_FC2[n].w_optim.lr = lr
+			self.W_FC2[n].b_optim.lr = lr
 
 
 	def forward(self, index_list, target_list, phase='train'):
@@ -74,15 +93,16 @@ class Decoder_model:
 			X+= self.PE[:len(index_list)]
 			context_size = X.shape[0]
 
-		X_sublayer = self.W_attention(X)
-		X_sublayer = self.MH_attention(X_sublayer, phase)
-		X_sublayer = self.W_heads_projection(X_sublayer)
-		X = self.Attention_LayerNorm(X + X_sublayer, phase)
+		for n in range(self.N):
+			X_sublayer = self.W_attention[n](X)
+			X_sublayer = self.MH_attention[n](X_sublayer, phase)
+			X_sublayer = self.W_heads_projection[n](X_sublayer)
+			X = self.Attention_LayerNorm[n](X + X_sublayer, phase)
 
-		X_sublayer = self.W_FC1(X)
-		X_sublayer = self.activation(X_sublayer)
-		X_sublayer = self.W_FC2(X_sublayer)
-		X = self.FC_LayerNorm(X + X_sublayer, phase)
+			X_sublayer = self.W_FC1[n](X)
+			X_sublayer = self.activation[n](X_sublayer)
+			X_sublayer = self.W_FC2[n](X_sublayer)
+			X = self.FC_LayerNorm[n](X + X_sublayer, phase)
 
 		X = self.TE.linear(X)
 		self.Output_token_probabilities = string_softmax(X, context_size)
@@ -94,9 +114,9 @@ class Decoder_model:
 			return loss_value
 		else:
 			# return the indexes with highest probability for tokens in vocabulary
-			# return np.argmax(self.Output_token_probabilities[-1], axis=1)
-			return np.random.choice(range(self.vocabulary_size), \
-				p=self.Output_token_probabilities[-1].ravel())
+			return np.argmax(self.Output_token_probabilities[-1]) #, axis=1)
+			# return np.random.choice(range(self.vocabulary_size), \
+			# 	p=self.Output_token_probabilities[-1].ravel())
 
 	def backward(self):
 		dl = self.Output_token_probabilities
@@ -105,17 +125,18 @@ class Decoder_model:
 
 		dl, TE_grad = self.TE.linear_backward(dl)
 
-		dl = self.FC_LayerNorm.backward(dl)
-		dl = self.W_FC2.backward(dl)
-		dl = self.activation.backward(dl)
-		dl = self.W_FC1.backward(dl)
-		dl += self.residual_backprop
+		for n in range(self.N):
+			dl = self.FC_LayerNorm[n].backward(dl)
+			dl = self.W_FC2[n].backward(dl)
+			dl = self.activation[n].backward(dl)
+			dl = self.W_FC1[n].backward(dl)
+			dl += self.residual_backprop
 
-		dl = self.Attention_LayerNorm.backward(dl)
-		dl = self.W_heads_projection.backward(dl)
-		dl = self.MH_attention.backward(dl)
-		dl = self.W_attention.backward(dl)
-		dl += self.residual_backprop
+			dl = self.Attention_LayerNorm[n].backward(dl)
+			dl = self.W_heads_projection[n].backward(dl)
+			dl = self.MH_attention[n].backward(dl)
+			dl = self.W_attention[n].backward(dl)
+			dl += self.residual_backprop
 
 		self.TE.update_weights(dl, TE_grad)
 		
