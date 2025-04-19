a2769ef51eb3e6a4984a52c676836faad38f7368
SLotAbr <49324598+SLotAbr@users.noreply.github.com>
2025-04-20 02:51:23 +0300

    a new data organization

--- main_loop.py
+++ main_loop.py
@@ -23,12 +23,12 @@ if len(os.listdir(path=save_folder)) != 0:
 	with open(save_folder+'model_decription.pkl', 'rb') as f:
 		context_size, d_model, H, N, optim_param = pickle.load(f)
 	with open(save_folder+'iteration_param.pkl', 'rb') as f:
-		s, step_num, loss, lr, target_lr, lr_step = pickle.load(f)
+		data_lenght, step_num, loss, lr, target_lr, lr_step = pickle.load(f)
 
 	model = Decoder_model(context_size, vocabulary_size, d_model, H, N, optim_param)
 	model.restore_parameters(save_folder)
 else:
-	s, step_num, loss = 0, 0, 0
+	step_num, loss = 0, 0
 	context_size, d_model, H, N = 128, 64, 2, 6 # 32, 64, 2, 3
 	target_lr = 1e-6
 	optim_param = [target_lr, 0.9, 0.98] # lr, b1, b2
@@ -51,42 +51,56 @@ else:
 
 	model = Decoder_model(context_size, vocabulary_size, d_model, H, N, optim_param)
 
-lr_decay_threshold = 120000
-# lr = target_lr / 100
-lr = target_lr
+lr_decay_threshold = 150000
+lr = target_lr / 100
+# lr = target_lr
 lr_step = (target_lr - lr) / lr_decay_threshold
 print('preparation\'s complete!')
 
+train_index, train_target = [], []
+for s in range(0, source_lenght-1, context_size):
+	train_index.append(
+		[letter_transform[letter] for letter in source[s:s+context_size]]
+	)
+	train_target.append(
+		[letter_transform[letter] for letter in source[s+1:s+context_size+1]]
+	)
+data_lenght = len(train_index)
+
 while True:
-	if s+context_size+1 >= source_lenght or step_num == 0:
-		s=0
+	if step_num > data_lenght:
+		step_num = 0
 
-	index_list = [letter_transform[letter] for letter in source[s:s+context_size]]
-	target_list = [letter_transform[letter] for letter in source[s+1:s+context_size+1]]
-
-	loss_value = model.forward(index_list,target_list, phase='train')
+	loss_value = model.forward(
+		train_index[step_num], 
+		train_target[step_num], 
+		phase='train'
+	)
 	loss = loss * 0.999 + loss_value * 0.001
 	model.backward()
 
 	if step_num%1000 == 0:
 		model.save_parameters(save_folder)
 		with open(save_folder+'iteration_param.pkl', 'wb') as f:
-			pickle.dump([s, step_num, loss, lr, target_lr, lr_step], f)
+			pickle.dump([data_lenght, step_num, loss, lr, target_lr, lr_step], f)
 
 		index_list = [np.random.randint(0, vocabulary_size)]
 		target_list = []
 		for l in range(128):
-			index_list.append(model.forward(index_list,target_list, phase='eval'))
+			index_list.append(
+				model.forward(index_list,target_list, phase='eval')
+			)
 		
 		text_example = ' '.join(indexes_transform[i] for i in index_list)
 		print('--------\n %s \n--------' % (text_example, ))
 		print('iter %d, loss: %f, lr: %g' % (step_num, loss, lr))
 
-	# if step_num <= lr_decay_threshold:
-	# 	lr += lr_step
-	# else:
-	# 	lr -= lr_step
-	# model.change_lr(lr)
+	if step_num <= lr_decay_threshold:
+		lr += lr_step
+	elif lr <= target_lr/100:
+		pass
+	else:
+		lr -= lr_step
+	model.change_lr(lr)
 
 	step_num += 1
-	s += context_size
